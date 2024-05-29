f6c2cce82392bd64877a3c9a35edbe130e9165b6
Peekhonsoay <perawat1122kattakarn@gmail.com>
2024-05-30 01:32:01 +0700

    change host name to 0.0.0.0

--- product-catalog/server.js
+++ product-catalog/server.js
@@ -168,6 +168,6 @@ app.get('/api/products/:id', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
